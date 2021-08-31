1e449af4b92e9bc334cf347bd5bf8c7d48699d15
estevaoreis25 <estevaoreis1587@gmail.com>
2021-08-31 15:06:13 -0300

    the initial Typescript compiler settings have been made

---
+++ app/models/negociacao.ts
@@ -0,0 +1,15 @@
+export class Negociacao {
+  #data;
+  #quantidade;
+  #valor;
+
+  constructor(data, quantidade, valor){
+    this.#data = data;
+    this.#quantidade = quantidade;
+    this.#valor = valor;
+  }
+
+  get volume(){
+    return this.#quantidade * this.#valor;
+  }
+}
