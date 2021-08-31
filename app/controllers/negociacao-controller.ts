2e64e5af5c050d82dfd42c56cbd0906ab7b63901
estevaoreis25 <estevaoreis1587@gmail.com>
2021-08-31 16:43:46 -0300

    Refactoring the method "adiciona" and converting input values to the correct types

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,12 +12,21 @@ export class NegociacaoController {
 
   }
 
-  adiciona() {
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+    console.log(negociacao);
+  }
+
+  criaNegociacao(): Negociacao{
+    const exp = /-/g;
+    const date = new Date(this.inputData.value.replace(exp, ','));
+    const quantidade = parseInt(this.inputQuantidade.value)
+    const valor = parseFloat(this.inputValor.value);
     const negociacao = new Negociacao(
-      this.inputData.value, 
-      this.inputQuantidade.value, 
-      this.inputValor.value)
-      console.log(negociacao);
+      date, 
+      quantidade, 
+      valor)
+    return negociacao;
   }
   
 }
