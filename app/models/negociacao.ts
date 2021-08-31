fca51c04deafaf262710cb15c2d3bcae20c484dd
estevaoreis25 <estevaoreis1587@gmail.com>
2021-08-31 16:22:57 -0300

    Typing variables, modifying Trading constructor and creating the trading controller

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,15 +1,27 @@
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
+  private _data: Date; 
+  private _quantidade: number;
+  private _valor: number;
 
-  constructor(data, quantidade, valor){
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
+  constructor(data: Date, quantidade: number, valor: number){
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
+
+  get quantidade(){
+    return this._quantidade;
+  }
+
+  get data(){
+    return this._data;
+  }
+
+  get valor(){
+    return this._valor;
   }
 
   get volume(){
-    return this.#quantidade * this.#valor;
+    return this._quantidade * this._valor;
   }
 }
